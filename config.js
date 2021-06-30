a70640e1d005b8a008fcb5a7ccded590984840a6
thienhuu07 <86707162+thienhuu07@users.noreply.github.com>
2021-06-30 14:49:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Đúng òy em yêu anh <3',
-    btnNo: 'Không, Bạn trai à :3',
+    btnNo: 'Không, Bạn trai à! :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Send My Doctor <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
