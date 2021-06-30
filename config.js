b4147eb002f31a6b3f3e288cbc1dbdb4c909621c
thienhuu07 <86707162+thienhuu07@users.noreply.github.com>
2021-06-30 19:13:34 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Công chúa ơi!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đấy là thương.`,
+    introDesc: `Em có biết em khác những vì sao ở điểm nào không? Sao ở trên trời, em ở trong tim anh.`,
     btnIntro: 'Hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Đúng òy em yêu anh <3',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy... ',
+    btnYes: 'Đúng òy! Em yêu anh <3',
     btnNo: 'Không, Bạn trai à! :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Send My Doctor <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    btnReply: 'Send My Doctor.',
+    reply: 'Yêu thì yêu mà không yêu thì yêuuuuuuuu',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Đừng giả bộ giận anh nữa nha Công chúa.',
-    btnAccept: 'Dạaaaaaaaaa!!!!!',
+    messDesc: 'Công chúa đừng "giả bộ" giận anh nữa nhaaaa.',
+    btnAccept: 'Tha cho anh đó đồ đáng ghét!',
     messLink: 'https://www.facebook.com/messages/t/100016536348801'
 }
